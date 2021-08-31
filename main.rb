d3c85a7de551160c6d46e5d5407748349c1e7195
yasuda.shunsuke <yasshun1229.com>
2021-08-31 10:25:52 +0000

    add main.rb

---
+++ main.rb
@@ -0,0 +1,10 @@
+require "./animal"
+require "./thinkable"
+require './human'
+
+# インスタンスの作成
+tanaka = Human.new("田中太郎", 25, "電車")
+suzuki = Human.new("鈴木次郎", 30, "野球")
+sato   = Human.new("佐藤花子", 20, "映画")
+
+# メソッドを実行
