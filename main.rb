92923d2c22265acd3058caf123f509268c9446ee
yasuda.shunsuke <yasshun1229.com>
2021-09-01 05:43:16 +0000

    main.rb

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
-require "./animal"
-require "./thinkable"
 require './human'
 
-# インスタンスの作成
+# インスタンスの作成と変数の代入
 tanaka = Human.new("田中太郎", 25, "電車")
 suzuki = Human.new("鈴木次郎", 30, "野球")
 sato   = Human.new("佐藤花子", 20, "映画")
 
 # メソッドを実行
+tanaka.say()
+tanaka.think()
+suzuki.say()
+suzuki.think()
+sato.say()
+sato.think()
